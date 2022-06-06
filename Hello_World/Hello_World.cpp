be4c80f6d717926f55d514fcee5e7f7d216af77c
XVND83 <XVND83@XVND83-Z14G8>
2022-06-06 21:12:11 +0200

    gitignore filew updated, HelloWorld done

--- Hello_World/Hello_World.cpp
+++ Hello_World/Hello_World.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+﻿#include <iostream>
+#include <cstring>
 
-int main()
+
+int main(int argc, char* argv[]) 
+/* alternatywny zapis
+ * int main(int argc, char** argv, char** envp)
+ * Gdzie envp jest tablicą na zmienne środowiskowe
+*/
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Argument Count (argc) = " << argc << std::endl;
+    std::cout << "Let's see what are the arguments (argv)\n";
+
+    for (int i = 0; i < argc; ++i) {
+        std::cout << "Argument number " << i+1 << ": " << argv[i] << std::endl;
+    }
+    return 0;
 }
